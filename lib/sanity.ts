97b8a065894c6beb536273d02d668d51ace5f785
NoeDbg <debregeasn@gmail.com>
2026-03-11 08:00:14 -0500

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,13 +1,16 @@
 import { createClient } from '@sanity/client';
 
-export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2024-07-01',
-  useCdn: false,
-  token: process.env.SANITY_API_TOKEN
-});
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
-export const sanityEnabled =
-  Boolean(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) &&
-  Boolean(process.env.NEXT_PUBLIC_SANITY_DATASET);
+export const sanityEnabled = Boolean(projectId) && Boolean(dataset);
+
+export const sanityClient = sanityEnabled
+  ? createClient({
+      projectId,
+      dataset,
+      apiVersion: '2024-07-01',
+      useCdn: false,
+      token: process.env.SANITY_API_TOKEN
+    })
+  : null;
